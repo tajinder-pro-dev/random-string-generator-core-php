baa98a7aae4db4909751afa7ef57e6cdb521b1f7
Tajinder Singh <tajindersingh70@gmail.com>
2025-03-05 21:16:41 +0530

    update code

--- random-string/logic.php
+++ random-string/logic.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         <?php endif; ?>
             <br>
-        <a href="index.html">Go Back to Generate Another String</a>
+        <a href="index.html">Go Back</a>
     </div>
 </body>
 </html>
